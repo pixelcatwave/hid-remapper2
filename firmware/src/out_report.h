1822d8e20f1f9fb24c9456c9828462d25b527814
pixelcatwave <96803098+pixelcatwave@users.noreply.github.com>
2025-11-29 20:37:09 -0500

    Update out_report.h

--- firmware/src/out_report.h
+++ firmware/src/out_report.h
@@ -3,6 +3,17 @@
 
 #include <stdint.h>
 
+// Nuance PowerMic-style 16-bit button mask bits
+#define PM_BTN_TRANSCRIBE    0x0001
+#define PM_BTN_TABBACKWARD   0x0002
+#define PM_BTN_DICTATE       0x0004
+#define PM_BTN_TABFORWARD    0x0008
+#define PM_BTN_REWIND        0x0010
+#define PM_BTN_FASTFORWARD   0x0020
+#define PM_BTN_STOPPLAY      0x0040
+#define PM_BTN_CUSTOMLEFT    0x0080
+#define PM_BTN_ENTERSELECT   0x0100
+
 enum class OutType : int8_t {
     OUTPUT = 0,
     GET_FEATURE = 1,
